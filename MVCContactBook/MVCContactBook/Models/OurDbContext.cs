b825f3d488b516e5061a9c10167fda77f143372f
RashedBabu <rashedbabufal@gmail.com>
2018-04-12 16:10:07 +0600

    first commit

---
+++ MVCContactBook/MVCContactBook/Models/OurDbContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+namespace MVCContactBook.Models
+{
+    public class OurDbContext:DbContext
+    {
+        public DbSet<UserAccount> userAccounts { get; set; }
+    }
+}
---
+++ MVCContactBook/MVCContactBook/Models/UserAccount.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace MVCContactBook.Models
+{
+    public class UserAccount
+    {
+        [Key]
+        public int UserID { get; set; }
+
+        [Required(ErrorMessage ="Frist name is Required")]
+        public string FristName { get; set; }
+
+        [Required(ErrorMessage = "Last name is Required")]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Email ID is Required")]
+
+        public string EmailID { get; set; }
+
+        [Required(ErrorMessage = "Password is Required")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+    }
+}
